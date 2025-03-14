f6e386861d1d746cdc65e0304954b0e45397493d
olivertransf <olivertransf@gmail.com>
2025-03-13 21:11:58 -0700

    Inital

---
+++ main.ts
@@ -0,0 +1,76 @@
+import { App, Plugin, PluginSettingTab, Setting, Modal, Notice } from 'obsidian';
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// Add a simple command
+		this.addCommand({
+			id: 'open-modal',
+			name: 'Open Modal',
+			callback: () => {
+				new SampleModal(this.app).open();
+			}
+		});
+
+		// Add settings tab
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		this.contentEl.setText('Hello, Obsidian!');
+	}
+
+	onClose() {
+		this.contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Custom Setting')
+			.setDesc('Enter a value')
+			.addText(text => text
+				.setPlaceholder('Type here...')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
